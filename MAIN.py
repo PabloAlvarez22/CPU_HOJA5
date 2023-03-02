b0b7a6ae9df5a95d77fd2f4bc01912a3cf21a9a6
Pablo Alvarez <pabloalv567@gmail.com>
2023-03-01 22:41:11 -0600

    Creación del main

---
+++ MAIN.py
@@ -0,0 +1,12 @@
+import statistics
+from random import randint
+from decimal import Decimal
+processingTime = []
+
+class Simulating:
+    def __init__(self, Key, Instructions, Memory):
+        self.Key = Key
+        self.Instructions = Instructions
+        self.Memory = Memory
+
+
